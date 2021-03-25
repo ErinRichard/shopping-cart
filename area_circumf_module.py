d2e4cc503f0233d75295a25f174a69e6ad37aa35
ErinRichard <richard.erind@gmail.com>
2021-03-24 23:39:00 -0500

    first commit

---
+++ area_circumf_module.py
@@ -0,0 +1,25 @@
+from math import floor as f, pi as p
+
+def square_footage(l,w):
+    """ 
+    the square_footage(l,w) function calculates square footage 
+    user needs to input length and width
+    square footage = length * width
+    """
+    l = l
+    w = w
+    area = l * w
+    return area
+
+
+
+
+def circumf(r):
+    """
+    Function circumf(r) calculates circumference of a circle based on radius
+    circumference = 2 * pi * radius
+    User needs to input radius
+    """
+    c = r * p * 2
+    return c
+
